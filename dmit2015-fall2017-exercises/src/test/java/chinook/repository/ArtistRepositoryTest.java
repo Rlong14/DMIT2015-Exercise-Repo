22f838823a6e8aa37eb33747e7a6af076306ede1
rlong14 <rlong14@studentmail.nait.ca>
2017-09-18 16:52:25 -0600

    fixed one small thing

--- dmit2015-fall2017-exercises/src/test/java/chinook/repository/ArtistRepositoryTest.java
+++ dmit2015-fall2017-exercises/src/test/java/chinook/repository/ArtistRepositoryTest.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.ejb.EJB;
 
 import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
