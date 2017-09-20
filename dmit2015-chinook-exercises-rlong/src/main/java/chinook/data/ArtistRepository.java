57aad433e90c1fcdf0453d097f6507caa367953b
rlong14 <rlong14@studentmail.nait.ca>
2017-09-20 16:48:25 -0600

    Created new chinook database exercise project

---
+++ dmit2015-chinook-exercises-rlong/src/main/java/chinook/data/ArtistRepository.java
@@ -0,0 +1,24 @@
+package chinook.data;
+
+import java.util.List;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+
+import chinook.model.Artist;
+
+@Stateless
+@LocalBean
+public class ArtistRepository {
+
+	@Inject
+	private EntityManager em;
+	
+	public List<Artist> findAll(){
+		return em.createQuery("SELECT a FROM Artist a").getResultList();
+		// return em.createQuery("FROM Artist a").getResultList();
+		
+	}
+}
